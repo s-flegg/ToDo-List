465adaeecd0142c7b8fe6c19872d66fafd09511f
fireKG0834 <nqvq0834@leeds.ac.uk>
2025-11-03 12:51:40 +0000

    added files

---
+++ add_task.py
@@ -0,0 +1,15 @@
+def add_task(task_list, title, description, due_date, priority):
+    '''
+    Add a new task to the task list.
+
+    Parameters:
+        task_list (list): The list of all tasks
+        title (str): The title of the task
+        description (str): The task description
+        due_date (obj): The datetime.datetime that the task is due
+        priority (int): Priority level from 1-5, 1 is most important
+
+    Returns:
+        list: Updated task list with the new task added
+    '''
+    pass
---
+++ remove_task.py
@@ -0,0 +1,13 @@
+def remove_task(task_list, title):
+    '''
+    Add a new task to the task list.
+
+    Parameters:
+        task_list (list): The list of all tasks
+        title (str): The title of the task
+
+    Returns:
+        list: Updated task list with the new task added
+    '''
+    pass
+
