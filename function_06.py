b0c99bae001fc555d439bb03d024fcd5a618e2d7
rhnq0311 <rhnq0311@leeds.ac.uk>
2025-11-06 17:10:59 +0000

    edits to save list function

--- function_06.py
+++ function_06.py
@@ -1,3 +1,6 @@
 def save_list(filename, task_list):
+    filename = input("Enter filename, ending in .py: ")
     with open(filename, "w") as f:
-        f.write(task_list)
+        f.write(str(task_list))
+
+
